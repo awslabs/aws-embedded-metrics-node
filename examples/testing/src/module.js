509c875e39e634c659443c05b3a7b152560dfae8
Jared Nance <jarnance@amazon.com>
2020-03-16 07:53:33 -0700

    add unit testing examples

---
+++ examples/testing/src/module.js
@@ -0,0 +1,20 @@
+const { metricScope, createMetricsLogger, Unit } = require("aws-embedded-metrics");
+
+const usingScope = metricScope(metrics => 
+    async (event, ctx) => {
+        metrics.putDimensions({ Service: "Aggregator" });
+        metrics.putMetric("EventCount", event.count, Unit.Count);
+        metrics.setProperty("AccountId", event.accountId);
+        metrics.setProperty("RequestId", ctx.requestId);
+    });
+
+const usingCreateLogger = async (event, ctx) => {
+    const metrics = createMetricsLogger();
+    metrics.putDimensions({ Service: "Aggregator" });
+    metrics.putMetric("EventCount", event.count, Unit.Count);
+    metrics.setProperty("AccountId", event.accountId);
+    metrics.setProperty("RequestId", ctx.requestId);
+    await metrics.flush();
+};
+    
+module.exports = { usingScope, usingCreateLogger };
