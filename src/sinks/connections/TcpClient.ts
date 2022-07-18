782676b6ee7d6304c1903e9beb5ff066b6ed755e
Mark Kuhn <kuhn.mark@outlook.com>
2022-07-18 15:36:24 -0700

    rm unused socket interface (#123)
    * fix socket writable typo
    * rm unused interface
    
    Co-authored-by: Mark Kuhn <kuhnmar@amazon.com>

--- src/sinks/connections/TcpClient.ts
+++ src/sinks/connections/TcpClient.ts
@@ -18,14 +18,9 @@ import { LOG } from '../../utils/Logger';
 import { IEndpoint } from './IEndpoint';
 import { ISocketClient } from './ISocketClient';
 
-interface SocketExtended extends net.Socket {
-  writeable: boolean;
-  readyState: string;
-}
-
 export class TcpClient implements ISocketClient {
   private endpoint: IEndpoint;
-  private socket: SocketExtended;
+  private socket: net.Socket;
 
   constructor(endpoint: IEndpoint) {
     this.endpoint = endpoint;
@@ -35,7 +30,7 @@ export class TcpClient implements ISocketClient {
       .setTimeout(5000) // idle timeout
       .on('timeout', () => this.disconnect('idle timeout'))
       .on('end', () => this.disconnect('end'))
-      .on('data', data => LOG('TcpClient received data.', data)) as SocketExtended;
+      .on('data', data => LOG('TcpClient received data.', data));
   }
 
   public async warmup(): Promise<void> {
@@ -50,7 +45,7 @@ export class TcpClient implements ISocketClient {
     // ensure the socket is open and writable
     await this.waitForOpenConnection();
 
-    await new Promise((resolve, reject) => {
+    await new Promise<void>((resolve, reject) => {
       const onSendError = (err: Error): void => {
         LOG('Failed to write', err);
         reject(err);
@@ -79,13 +74,13 @@ export class TcpClient implements ISocketClient {
   }
 
   private async waitForOpenConnection(): Promise<void> {
-    if (!this.socket.writeable || this.socket.readyState !== 'open') {
+    if (!this.socket.writable || this.socket.readyState !== 'open') {
       await this.establishConnection();
     }
   }
 
   private async establishConnection(): Promise<void> {
-    await new Promise((resolve, reject) => {
+    await new Promise<void>((resolve, reject) => {
       const onError = (e: Error): void => {
         // socket is already open, no need to connect
         if (e.message.includes('EISCONN')) {
