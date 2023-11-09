d0a37f2d57c1ca77b7b415dd74d4190c97e0aa93
Mark Kuhn <kuhnmar@amazon.com>
2023-11-09 15:13:30 -0800

    Change ConsoleSink depending on Lambda log format (#158)
    * Change ConsoleSink depending on Lambda log format
    
    - use console.log as default
    - use Console() when in JSON format

--- src/sinks/__tests__/ConsoleSink.test.ts
+++ src/sinks/__tests__/ConsoleSink.test.ts
@@ -6,6 +6,10 @@ beforeEach(() => {
   console.log = jest.fn();
 });
 
+afterEach(() => {
+  delete process.env.AWS_LAMBDA_LOG_FORMAT;
+});
+
 test('accept serializes and writes result to stdout', () => {
   // arrange
   const expected = faker.random.alphaNumeric(20);
@@ -19,7 +23,27 @@ test('accept serializes and writes result to stdout', () => {
   sink.accept(MetricsContext.empty());
 
   // assert
+  expect(sink.console).toBe(console);
   expect(console.log).toBeCalledWith(expected);
+});
+
+test('accept serialized and writes result to stdout when lambda log format is JSON', () => {
+  // arrange
+  process.env.AWS_LAMBDA_LOG_FORMAT = 'JSON';
+  const expected = faker.random.alphaNumeric(20);
+  const serializer: any = {
+    serialize: jest.fn(() => [expected]),
+  };
+
+  const sink = new ConsoleSink(serializer);
+  const spy = jest.spyOn(sink.console, 'log');
+
+  // act
+  sink.accept(MetricsContext.empty());
+
+  // assert
+  expect(sink.console).not.toBe(console);
+  expect(spy).toBeCalledWith(expected);
 });
 
 test('accept writes multiple messages to stdout', () => {
@@ -36,6 +60,28 @@ test('accept writes multiple messages to stdout', () => {
   sink.accept(MetricsContext.empty());
 
   // assert
+  expect(sink.console).toBe(console);
   expect(console.log).toBeCalledTimes(expectedMessages);
-  expected.forEach(e => expect(console.log).toBeCalledWith(e));
+  expected.forEach((e) => expect(console.log).toBeCalledWith(e));
+});
+
+test('accept writes multiple messages to stdout when lambda log format is JSON', () => {
+  // arrange
+  process.env.AWS_LAMBDA_LOG_FORMAT = 'JSON';
+  const expectedMessages = faker.datatype.number({ min: 2, max: 100 });
+  const expected = new Array(expectedMessages).fill(null).map(() => faker.random.alphaNumeric(20));
+  const serializer: any = {
+    serialize: jest.fn(() => expected),
+  };
+
+  const sink = new ConsoleSink(serializer);
+  const spy = jest.spyOn(sink.console, 'log');
+
+  // act
+  sink.accept(MetricsContext.empty());
+
+  // assert
+  expect(sink.console).not.toBe(console);
+  expect(spy).toBeCalledTimes(expectedMessages);
+  expected.forEach((e) => expect(spy).toBeCalledWith(e));
 });
